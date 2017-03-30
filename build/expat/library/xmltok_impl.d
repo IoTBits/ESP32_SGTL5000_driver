1c7c0211cf191fc61e771d228d383e5b7475798f
iot-bits <hello@pratikpanda.com>
2017-03-30 15:51:01 +0530

    Added config APIs

--- build/expat/library/xmltok_impl.d
+++
@@ -1,2 +0,0 @@
-library/xmltok_impl.o: \
- /home/esp32-dev/Desktop/idf_new/esp-idf/components/expat/library/xmltok_impl.c
--- build/expat/library/xmltok_ns.d
+++
@@ -1,2 +0,0 @@
-library/xmltok_ns.o: \
- /home/esp32-dev/Desktop/idf_new/esp-idf/components/expat/library/xmltok_ns.c
